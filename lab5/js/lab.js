7e63b8543cb215f2b68594dbeaed8016c825d3a4
Jessica Zogaric <59582785+kickflipyeah@users.noreply.github.com>
2022-10-17 22:47:25 -0700

    lab 5
    added lab 5 index, javascript, and img

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/**
+ * Author:    Jessica Zogaric
+ * Created:   17.10.2022
+ *
+ * Lab 5: A JavaScript Program
+ **/
+
+//Define Variables
+ var make = "Honda";
+ var model = "Civic";
+ var color = "Gray";
+ var year = "2019";
+
+//calculations
+var age = 2022 - year;
+
+//output info
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");

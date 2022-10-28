2738f7970a11c4ce8d9ba5b7e94fac4c1c676846
kickflipyeah <59582785+kickflipyeah@users.noreply.github.com>
2022-10-27 23:41:44 -0700

    added code and such for lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -4,3 +4,24 @@
  * ART 101
  *
  **/
+
+function numAdd(x){
+    return (x + x) - 8;
+    console.log("numAdd", );
+}
+
+console.log("test numAdd", numAdd(5));
+console.log("test numAdd", numAdd(12));
+
+numArray = [3, 5, 8, 9, 12, 26, 79];
+console.log("numArray", numArray);
+
+var withAdd = numArray.map(numAdd);
+//will display results of numAdd after performed on numArray
+console.log("function numAdd performed on numArray", withAdd);
+
+var withAdd = numArray.map(function(x){
+    return (x * x) + (x - 2);
+})
+
+console.log("Array multiplied by itself added to itself minus 2", withAdd);

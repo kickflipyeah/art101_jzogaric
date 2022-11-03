8bc431498444a168532c29850578d605f3a16ff7
Jessica Zogaric <59582785+kickflipyeah@users.noreply.github.com>
2022-11-03 01:41:19 -0700

    lab 10
    lab 10 done

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -4,3 +4,13 @@
  * ART 101
  *
  **/
+
+var userOutput = document.getElementById("output");
+
+  var userInput = document.getElementById("my-button").addEventListener("click", function() {
+    var userInput = document.getElementById("user-name").value;
+    var userInput = userInput.toLowerCase().split("").sort().join("");
+    console.log(userInput);
+    userOutput.innerHTML = "Here is your new username:  " + userInput;
+
+  });

dc8a83afefa255015f6717808a2bf1ab51d6f2b2
Jessica Zogaric <59582785+kickflipyeah@users.noreply.github.com>
2022-11-16 22:36:01 -0800

    lab 14 added

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -5,8 +5,18 @@
  *
  **/
 
-$('.p').append("<button id='my-button'> Please press me!");
+$('#p').append("<button id='my-button'> Please press me!");
+$('#p1').append("<button id='my-button1'> Please press me!");
+$('#p2').append("<button id='my-button2'> Please press me!");
 
-$("button").click(function(){
-  $('.p').toggleClass("special");
+$("#my-button").click(function(){
+  $('#p').toggleClass("special");
+});
+
+$("#my-button1").click(function(){
+  $('#p1').toggleClass("special");
+});
+
+$("#my-button2").click(function(){
+  $('#p2').toggleClass("special");
 });
